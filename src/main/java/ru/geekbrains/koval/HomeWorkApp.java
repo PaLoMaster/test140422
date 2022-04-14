75ee41f7fb28eaba4b0664646cce940e4e0ec203
HP <orange59rus@mail.ru>
2022-04-14 14:34:22 +0300

    initial commit

--- src/main/java/ru/geekbrains/koval/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/koval/HomeWorkApp.java
@@ -5,6 +5,11 @@ public class HomeWorkApp {
 
     }
 
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
 
 
 
