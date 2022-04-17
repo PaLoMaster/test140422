6a13bd6a717b4813a3c84b2cb94928b7fe646a5f
PaLoMaster <renat.programmizm@yandex.ru>
2022-04-17 22:26:28 +0400

    Debug & improvements

--- src/main/java/ru/geekbrains/koval/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/koval/HomeWorkApp.java
@@ -3,8 +3,8 @@ package ru.geekbrains.koval;
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
-        compareNumbers();
-        checkSummSign();
+        compareNumbers(70, 42);
+        checkSummSign(2, 4);
     }
 
             public static void printThreeWords() {
@@ -12,9 +12,7 @@ public class HomeWorkApp {
                 System.out.println("Banana");
                 System.out.println("Apple");
             }
-    public static void checkSummSign() {
-        int a = 2;
-        int b = 4;
+    public static void checkSummSign(int a, int b) {
         if ((a + b) >= 0) {
             System.out.println("сумма положительна");
         } else {
@@ -22,19 +20,17 @@ public class HomeWorkApp {
         }
     }
 
-        public static void PrintColor () {
-            int value = 20;
+        public static void printColor(int value) {
             if (value <= 0) {
                 System.out.println("Красный");
-            } else if (value > 0 && value <= 100) {
+            } else if (value <= 100) {
                 System.out.println("Жёлтый");
             } else {
                 System.out.println("Зелёный");
             }
         }
 
-    public static void compareNumbers() {
-        int a = 70, b = 42;
+    public static void compareNumbers(int a, int b) {
         if (a >= b) {
             System.out.println("a>=b");
         } else{
